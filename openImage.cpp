820294411171f761b8efbef331fb095951e968ff
Vimlesh Kumar Kanaujiya <kanaujiyavimleshkumar@gmail.com>
2023-06-23 02:54:29 +0530

    Create openImage.cpp

---
+++ openImage.cpp
@@ -0,0 +1,18 @@
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main() {
+
+	string path = "image.jpg";
+	Mat img = imread(path);
+	imshow("Image", img);
+	waitKey(0);
+  
+  return 0;
+  
+}
